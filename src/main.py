5144bab40e9c60b4abdd511a5e46936ba33f9e23
luanwcs84 <124096843+luanwcs84@users.noreply.github.com>
2026-05-19 22:24:20 -0300

    feat: adiciona o estado degraded e gitignore

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import requests
 from fastapi import FastAPI
 from datetime import datetime, timezone
 
@@ -5,8 +6,29 @@ app = FastAPI(title="API de Agregação de Dados Climáticos e Geográficos")
 
 @app.get("/api/v1/health")
 async def health_check():
-    return {
-        "status": "healthy",
-        "versao": "1.0.0",
-        "timestamp": datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
-    }
+    current_timestamp = datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
+    
+    try:
+        request_response = requests.get("https://brasilapi.com.br/api/cptec/v1/cidade", timeout=5)
+        
+        if request_response.status_code == 200:
+            return {
+                "status": "healthy",
+                "versao": "1.0.0",
+                "timestamp": current_timestamp
+            }
+        else:
+            return {
+                "status": "degraded",
+                "versao": "1.0.0",
+                "timestamp": current_timestamp,
+                "motivo": "Serviço externo indisponível"
+            }
+        
+    except Exception:
+        return {
+            "status": "degraded",
+            "versao": "1.0.0",
+            "timestamp": current_timestamp,
+            "motivo": "Serviço externo indisponível"
+        }
